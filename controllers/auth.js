9319c8ef22e7e4980564c24ff41edd574010fbb5
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-08-28 18:48:28 -0500

    Sirviendo un directorio público y separando lógica

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const {response} = require('express');
+
+const crearUsuario = (req, res = response) => {
+
+    const {email, name, password } = req.body;
+    
+    return res.json({ok: true});
+}
+const loginUsuario = (req, res) => {
+    const {email, password } = req.body;
+    return res.json({
+        ok: true,
+        msg: 'Login de usuario / '
+    });
+}
+const revalidarToken = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: 'Renew'
+    });
+}
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+// console.log(process.env);
 
 
 
 //Crear el servidor / aplicación de express
+const app = express();
+
+//Directorio público
+app.use( express.static('public') );
+
+//cors
+app.use(cors());
+
+
+//Lectura y parseo del body
+
+app.use( express.json() );
+
+//Rutas
+app.use('/api/auth', require('./routes/auth')); 
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`);   
+});
