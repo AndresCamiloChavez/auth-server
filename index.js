2db888916499f38e1a6c83b570b9b01382787566
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-08-29 22:43:01 -0500

    Conexión a base de datoss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('./db/config');
 require('dotenv').config();
 // console.log(process.env);
 
@@ -7,6 +8,9 @@ require('dotenv').config();
 
 //Crear el servidor / aplicación de express
 const app = express();
+
+//Conexión a base de datos 
+dbConnection();
 
 //Directorio público
 app.use( express.static('public') );
