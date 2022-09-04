e9142de85304fc7253f0a4123a6d2f4e9008c599
ANDRESCZ <andres.cz.chavez@gmail.com>
2022-09-03 21:10:07 -0500

    Crando usuario en mongo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,77 @@
-const {response} = require('express');
-const {validationResult} = require('express-validator');
+const { response } = require("express");
+const { validationResult } = require("express-validator");
+const { db } = require("../models/Usuario");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
+  const { email, name, password } = req.body;
 
-    const {email, name, password } = req.body;
+  try {
+
+    console.log('se está creando el usuario');
+    //Verificar el email que no existe
+    const usuario = await Usuario.findOne({ email });
+    if(usuario){
+        return res.status(400).json({
+            ok: false,
+            msg: 'El usuario ya existe con ese email'
+        })
+    }
+
+    // Crear usuario con el modelo
+
+    const dbUser = new Usuario(req.body);
+
+    //Hasear la contraseña
+
+    //generar JWT
     
-    return res.json({ok: true});
-}
+
+    // Crear usuario de base de datos
+    await dbUser.save();
+
+    //Generar respuesta exitosa
+    console.log('valor del id del usuario  ', dbUser.id);
+    return res.status(201).json({
+        ok: true,
+        uid: dbUser.id,
+        name,
+    });
+
+
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ ok: true, msg: "Por favor hable con el administrador" });
+  }
+};
 const loginUsuario = (req, res = response) => {
+  // const errors = validationResult(req)
+  // console.log('mis errores', errors);
 
-    // const errors = validationResult(req)
-    // console.log('mis errores', errors);
-
-    // if(!errors.isEmpty()){
-    //     return res.status(400).json({
-    //         ok: false,
-    //         erros: errors.mapped()
-    //     });
-    // }
-    const {email, password } = req.body;
-    console.log(email, password);
-    return res.json({
-        ok: true,
-        msg: 'Login de usuario / '
-    });
-}
+  // if(!errors.isEmpty()){
+  //     return res.status(400).json({
+  //         ok: false,
+  //         erros: errors.mapped()
+  //     });
+  // }
+  const { email, password } = req.body;
+  console.log(email, password);
+  return res.json({
+    ok: true,
+    msg: "Login de usuario / ",
+  });
+};
 const revalidarToken = (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Renew'
-    });
-}
+  return res.json({
+    ok: true,
+    msg: "Renew",
+  });
+};
 
 module.exports = {
-    crearUsuario,
-    loginUsuario,
-    revalidarToken
-}
+  crearUsuario,
+  loginUsuario,
+  revalidarToken,
+};
